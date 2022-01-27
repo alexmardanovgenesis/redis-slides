ad7215fc9b2cf3efc1f99b5e9bd655828aa46d9f
Alexandr Mardanov <alexandr.mardanov@gmail.com>
2022-01-27 17:09:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://memo-slides.netlify.app/
---
+++ src/templates/slide.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+export default ({ data, transition }) => (
+  <div
+    style={transition && transition.style}
+    dangerouslySetInnerHTML={{ __html: data.slide.html }}
+  />
+);
+
+export const query = graphql`
+  query SlideQuery($index: Int!) {
+    slide(index: { eq: $index }) {
+      html
+      index
+    }
+  }
+`;
